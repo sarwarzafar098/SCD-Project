94ceca1def43a686dd55e26b91e4de87391d3ffe
sarwarzafar098 <sarwarzafar911@gmail.com>
2025-06-25 01:41:34 +0500

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,37 @@
+package task_reminder_app;
+
+import java.time.LocalDate;
+
+public class Task {
+    private String title;
+    private LocalDate dueDate;
+    private boolean completed;
+
+    public Task(String title, LocalDate dueDate) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.completed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void markCompleted() {
+        this.completed = true;
+    }
+
+    @Override
+    public String toString() {
+        return (completed ? "[✓] " : "[ ] ") + title + " (Due: " + dueDate + ")";
+    }
+}
+
